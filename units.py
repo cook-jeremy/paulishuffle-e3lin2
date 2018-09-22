3c3716fe06532eb72e3edb958ca3d8fa449c4d52
Jeremy Cook <jeremycook97@gmail.com>
2018-09-22 08:44:56 -0500

    initial commit

---
+++ units.py
@@ -0,0 +1,26 @@
+import unittest
+import numpy as np
+import math
+import diag
+
+I = np.matrix('1 0; 0 1')
+X = np.matrix('0 1; 1 0')
+Y = np.matrix('0 -1;1 0')*complex(0,1)
+Z = np.matrix('1 0; 0 -1')
+paulis = [I,X,Y,Z]
+
+class TestDiag(unittest.TestCase):
+    def test_trivial_decomp(self):
+        self.assertEqual(diag.decompose(0,I),[1,0,0,0])
+        self.assertEqual(diag.decompose(0,X),[0,1,0,0])
+        self.assertEqual(diag.decompose(0,Y),[0,0,1,0])
+        self.assertEqual(diag.decompose(0,Z),[0,0,0,1])
+
+    def test_trivial_pi(self):
+        self.assertEqual(np.allclose(diag.decompose(math.pi,I),[1,0,0,0]),True)
+        self.assertEqual(np.allclose(diag.decompose(math.pi,X),[0,1,0,0]),True)
+        self.assertEqual(np.allclose(diag.decompose(math.pi,Y),[0,0,1,0]),True)
+        self.assertEqual(np.allclose(diag.decompose(math.pi,Z),[0,0,0,1]),True)
+
+if __name__ == '__main__':
+    unittest.main()
