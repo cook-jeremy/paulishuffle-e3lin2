f75e1430e22fee337b1c080e645ab8521fe0fa31
Jeremy Cook <jeremycook97@gmail.com>
2018-10-21 19:41:57 -0500

    working on cuda code a bit

--- sample.cu
+++ sample.cu
@@ -1,7 +1,19 @@
+// reading a text file
 #include <iostream>
-#include <math.h>
 #include <stdio.h>
+#include <fstream>
+#include <math.h>
+#include <string>
+#include <vector>
+#include <sstream>
+using namespace std;
 
+struct matrix {
+    vector< vector<int> > coeff;
+    vector<int> sol;
+};
+
+/**
 int main(void) {
     // generate equation set
     
@@ -16,4 +28,47 @@ int main(void) {
     // pick random equation to create ...YYY...
     // apply e^{i\gamma C} to all equations that act on ...YYY...
     // take the trace with the |+> state to get observable
+}
+**/
+
+vector<int>* read_file(char* filename) {
+    string line;
+    ifstream myfile(filename);
+    vector< vector<int> > eqns;
+    vector<int> sol;
+    if (myfile.is_open()) {
+        while(getline(myfile,line)) {
+            //cout << line << '\n';
+            vector<int> eqn;
+            stringstream ss(line);
+            int i;
+            int counter = 0;
+            while (ss >> i) {
+                if(counter < 3) eqn.push_back(i);
+                else sol.push_back(i);
+                if(ss.peek() == ',') ss.ignore();
+                counter++;
+            }
+            eqns.push_back(eqn);
+        }
+        myfile.close();
+    }
+    else {
+        cout << "Unable to open file" << endl; 
+    }
+    for(int i = 0; i < eqns.size(); i++) {
+        cout << eqns.at(i).at(0) << " " << eqns.at(i).at(1) << " " << eqns.at(i).at(2) << " | " << sol.at(i) << endl;
+    }
+}
+
+int main(int argc, char **argv) {
+    // first arugment is equation file
+    if(argc < 2) {
+        cout << "not enough arguments, please specify equation file" << endl;
+        return 0;
+    }
+    cout << "eqn file: " << argv[1] << endl;
+    // read from file
+    read_file(argv[1]);
+    return 0;
 }
