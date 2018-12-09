4483539e3078d56006aeb8df90fe80d73f8715e0
MagicFridge <dyl32@cornell.edu>
2018-12-08 19:51:54 -0600

    fixed typo

--- gather_data.py
+++ gather_data.py
@@ -1,32 +1,34 @@
-import create_equations
-import numpy as np
+import os, sys
 
-username = "dliang27"
 
-num_vars = 20
-d_constraint = 30
-num_eqns = 45
-noise_p = 0
-nodes = 1
+def gather_data_helper(samples, f):
+    log_num_sample = int(f.readline())
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        results = map(int, line.split(","))
+        key = results[0]
+        if key not in samples:
+            samples[key] = (results[1], results[2])
+        else:
+            samples[key] = (samples[key][0] + results[1], + samples[key][1] + results[2])
 
-eqns_location = "equations/figure_eqns/equations"
-
-def init_sample():
-    create_equations.create_eqns(eqns_location, num_vars, d_constraint, num_eqns, noise_p)
-    gammalist = [0.2]
-    for gamma in gammalist:
-        f = open("paramlist", "w")
-        for i in range(nodes):
-	    f.write("sample.exe equations/figure_eqns/equations %g > output/tally%g_%d.o 2> output/tally%g_%d.e\n" % (gamma, gamma, i+1, gamma, i+1))
-            f.write("python exact.py %d %d %s %g > output/exact%g.o 2> output/exact%g.e\n" % (num_vars, num_eqns, eqns_location, gamma, gamma, gamma))
-	f.close()
-
-def sample():
-    pass
+def gather_data(gamma):
+    samples = dict() 
+    i = 0
+    file_location = "output/tally%g_%d.o" % (gamma, i+1)
+    while os.path.exists(file_location):
+        f = open(file_location, "r")
+        gather_data_helper(samples, f)
+        f.close()
+        i += 1
+        file_location = "output/tally%g_%d.o" % (gamma, i+1)
+    return samples
+      
 
 def main():
-    init_sample()
-
+    print(gather_data(0.2))
 
 if __name__ == '__main__':
     main()
